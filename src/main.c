0d1cb0746f400181b10c94f4a3f17cbec22a860b
benes-al <gryk_@hotmail.com>
2025-07-30 22:12:49 +0100

    fix: radix working

--- src/main.c
+++ src/main.c
@@ -6,48 +6,11 @@
 /*   By: benes-al <benes-al@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 13:56:55 by benes-al          #+#    #+#             */
-/*   Updated: 2025/07/30 21:04:22 by benes-al         ###   ########.fr       */
+/*   Updated: 2025/07/30 22:12:04 by benes-al         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-#if 1
-# include <stdio.h>
-void	print_stack(t_stack *stack)
-{
-	t_node *current;
-	
-	current = stack->top;
-	if(!stack->top)
-	{
-		write(1, "empty\n", 6);
-		return ;
-	}
-	printf("Stack (top to bottom):\n");
-	while (current)
-	{
-		printf("Value: %d | Index: %d\n", current->value, current->index);
-		current = current->next;
-	}
-}
-void	print_content(t_stack *stack_a, t_stack *stack_b)
-{
-	printf("Before operation stack_a:\n");
-	print_stack(stack_a);
-	printf("\nBefore operation stack_b:\n");
-	print_stack(stack_b);
-	printf("------------------------------\n");
-
-	sort_tokens(stack_a, stack_b);
-
-	printf("\nAfter operation stack_a:\n");
-	print_stack(stack_a);
-	printf("\nAfter operation stack_b:\n");
-	print_stack(stack_b);
-	printf("------------------------------\n");
-}
-#endif
 
 int	main(int argc, char **argv)
 {
@@ -64,8 +27,7 @@ int	main(int argc, char **argv)
 	parser (argc, argv, &stack_a);
 	if (is_stack_sorted(&stack_a))
 		exit (0);
-	//sort_tokens(&stack_a, &stack_b);
-	print_content(&stack_a, &stack_b); 
+	sort_tokens(&stack_a, &stack_b);
 	free_stack(&stack_a);
 	return (0);
 	
--- src/sorting/radix_sort.c
+++ src/sorting/radix_sort.c
@@ -6,7 +6,7 @@
 /*   By: benes-al <benes-al@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 21:27:49 by benes-al          #+#    #+#             */
-/*   Updated: 2025/07/30 21:10:13 by benes-al         ###   ########.fr       */
+/*   Updated: 2025/07/30 21:21:47 by benes-al         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ void	radix_sort(t_stack *stack_a, t_stack *stack_b)
 	int	max_bites;
 	
 	i = 0;
-	j = 0;
 	num_tokens = stack_a->size;
 	max_bites = calculate_max_bites(num_tokens - 1);
 	sort_index(stack_a);
 	while (i <= max_bites)
 	{
+		j = 0;
 		while (j < num_tokens)
 		{
 			if ((stack_a->top->index >> i) & 1)
@@ -36,8 +36,6 @@ void	radix_sort(t_stack *stack_a, t_stack *stack_b)
 		}
 		while(stack_b->top)
 			pa(stack_a, stack_b);
-		print_stack(stack_a);
-		print_stack(stack_b);
 		i++;
 	}
 } 
